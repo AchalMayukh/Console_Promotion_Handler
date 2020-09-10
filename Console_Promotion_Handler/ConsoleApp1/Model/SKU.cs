2c6f9b3649dae66617a36dcbd9cbfc7d0c841719
Achal Mayukh <achal_mayukh@outlook.com>
2020-09-11 00:29:21 +0530

    Adding required Models

---
+++ Console_Promotion_Handler/ConsoleApp1/Model/SKU.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionHandler.Model
+{
+    static class SKU
+    {
+        public static char[] IDs = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => ((char)i)).ToArray();
+    }
+}
---
+++ Console_Promotion_Handler/ConsoleApp1/Model/UnitPrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionHandler.Model
+{
+    class UnitPrice
+    {
+      
+        public static Dictionary<char, int> GetPrices()
+        {
+            Dictionary<char, int> quote = new Dictionary<char, int>();
+            
+            return quote;
+        }
+    }
+}
